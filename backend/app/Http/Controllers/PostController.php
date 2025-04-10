5148c0e03c6e22331c5d3f03587844fd8eac93c0
Zeromonster12 <152170025+Zeromonster12@users.noreply.github.com>
2025-04-10 13:10:09 +0200

    prepojenie na backend pre posty

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Post::select('image', 'title', 'body')->get();
+        return Post::select('id', 'image', 'title', 'body')->get();
     }
 
     public function show(Request $request)
